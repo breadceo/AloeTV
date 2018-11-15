e13f1257564605df7127c1c1ed786bdc7b17e6da
daewook <logue777@nate.com>
2018-11-16 01:51:24 +0900

    [time diff] ago 추가

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -13,6 +13,9 @@
 import { mapState } from 'vuex';
 // mapActions
 
+const moment = require('moment');
+const preciseDiff = require('moment-precise-range')(moment);
+
 export default {
   name: 'VideoCard',
   props: ['video'],
@@ -20,7 +23,24 @@ export default {
     ...mapState({
     }),
     ago: function ago() {
-      return '3 month ago';
+      const m1 = this.$moment(this.video.snippet.publishedAt);
+      const m2 = this.$moment();
+      const diff = preciseDiff(m1, m2, { returnObject: true });
+      if (diff.years > 0) {
+        return `${diff.years} years ago`;
+      } else if (diff.months > 0) {
+        return `${diff.months} months ago`;
+      } else if (diff.days > 0) {
+        if (diff.days > 7) {
+          return `${diff.days / 7} weeks ago`;
+        }
+        return `${diff.days} days ago`;
+      } else if (diff.hours > 0) {
+        return `${diff.hours} hours ago`;
+      } else if (diff.minutes > 0) {
+        return `${diff.minutes} minutes ago`;
+      }
+      return `${diff.seconds} seconds ago`;
     },
   },
   // methods: mapActions(''
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import moment from 'moment';
+import VueMomentJS from 'vue-momentjs';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App';
@@ -10,6 +12,7 @@ import router from './router';
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
+Vue.use(VueMomentJS, moment);
 
 /* eslint-disable no-new */
 new Vue({
