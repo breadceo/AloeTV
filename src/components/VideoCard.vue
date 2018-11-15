128e27e65060312bb9decd3610442485c47a6a5b
daewook <logue777@nate.com>
2018-11-16 01:50:51 +0900

    VideoCard 추가

---
+++ src/components/VideoCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="videocard">
+    <b-img :src=this.video.snippet.thumbnails.high.url fluid-grow
+     thumbnail alt="Responsive image" />
+    <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">{{ this.video.snippet.localized.title }}</h5>
+        <small class="text-muted">{{ this.ago }}</small>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+// mapActions
+
+export default {
+  name: 'VideoCard',
+  props: ['video'],
+  computed: {
+    ...mapState({
+    }),
+    ago: function ago() {
+      return '3 month ago';
+    },
+  },
+  // methods: mapActions(''
+  // }),
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="videolist">
     <b-list-group>
-      <b-list-group-item variant="light"
-      class="flex-column w-75 center-block">
-        <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">List group item heading</h5>
-          <small class="text-muted">3 days ago</small>
-        </div>
+      <b-list-group-item variant="light" class="flex-column w-50 center-block"
+      v-for="(video, index) in videos" v-bind:key="video.id" >
+        <video-card v-bind='{ video }' v-bind:index="index" v-bind:key="video.id" />
       </b-list-group-item>
     </b-list-group>
   </div>
@@ -15,19 +11,14 @@
 
 <script>
 import { mapState } from 'vuex';
-// mapActions
+import VideoCard from './VideoCard';
 
 export default {
   name: 'VideoList',
+  components: { VideoCard },
   computed: mapState({
     videos: state => state.videos.all,
   }),
-  // methods: mapActions(''
-  // }),
-  data() {
-    return {
-    };
-  },
   created() {
     this.$store.dispatch('videos/getAllVideos');
   },
