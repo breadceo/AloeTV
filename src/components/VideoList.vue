2efa588458f98eb445fecafa3926a854a829e06c
daewook <logue777@nate.com>
2018-11-15 16:24:28 +0900

    리스트 스타일링

---
+++ src/components/VideoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="videolist">
+    <b-list-group>
+      <b-list-group-item variant="light"
+      class="flex-column w-75 center-block">
+        <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">List group item heading</h5>
+          <small class="text-muted">3 days ago</small>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item variant="light"
+      class="flex-column w-75 center-block">
+        <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">List group item heading</h5>
+          <small class="text-muted">3 days ago</small>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item variant="light"
+      class="flex-column w-75 center-block">
+        <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">List group item heading</h5>
+          <small class="text-muted">3 days ago</small>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item variant="light"
+      class="flex-column w-75 center-block">
+        <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">List group item heading</h5>
+          <small class="text-muted">3 days ago</small>
+        </div>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoList',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.center-block {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Videos from '@/components/Videos';
+import VideoList from '@/components/VideoList';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Videos',
-      component: Videos,
+      name: 'VideoList',
+      component: VideoList,
     },
   ],
 });
