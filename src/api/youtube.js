c793b6418358ba352fb15b2c18599377b8586a40
daewook <logue777@nate.com>
2018-11-22 00:07:40 +0900

    spreadsheet 에서 읽어오게

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,21 +1,34 @@
 const axios = require('axios');
+const GoogleSpreadSheet = require('google-spreadsheet');
+const _ = require('lodash');
+const creds = require('./aloetv-222617-c3481ab5e923');
 
-const database = [
-  '6xJlnXOE_2o',
-  'ocO470rx-Bk',
-  'Mb19aQcLxBE',
-  '-AXC7UzodT0',
-  'E-T1gEeUqwA',
-  'mBEpIqtE8Pg',
-];
 const apiKey = 'AIzaSyAMyMZFjNns6Uu_RvuOJyVqQpfpKvh6sG0';
 
 export default {
   async fetchVideos() {
     try {
+      const docs = new GoogleSpreadSheet('1DQ_yIJ1AJWPLfmhJsLSoe3tkOsnhsg31k9knhsT12gQ');
+      await new Promise((resolve) => {
+        docs.useServiceAccountAuth(creds, () => {
+          docs.getInfo((err, info) => {
+            resolve(info.worksheets[0]);
+          });
+        });
+      });
+      let database = await new Promise((resolve) => {
+        docs.getRows(1, {
+          offset: 1,
+          limit: 9999,
+        }, (err, cells) => {
+          resolve(cells);
+        });
+      });
+      database = _.reverse(_.sortBy(database, ['publishtime']));
+
       const prs = [];
-      database.forEach((id) => {
-        const pr = axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&maxResults=1&key=${apiKey}`);
+      database.forEach((data) => {
+        const pr = axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${data.id}&maxResults=1&key=${apiKey}`);
         prs.push(pr);
       });
       const results = await Promise.all(prs);
