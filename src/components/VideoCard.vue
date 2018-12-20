cdac135d3879b575c7534bbbdd782c7dd8f2df48
daewook <logue777@nate.com>
2018-12-21 00:28:28 +0900

    vue-virtual-scroller 로 교체

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="videocard" v-on:click.stop="play(video.id)">
-    <b-img :src=this.video.snippet.thumbnails.high.url fluid-grow
-     thumbnail alt="Responsive image" />
+    <b-img v-if="this.$store.state.videos.playingId !== video.id"
+    :src=this.video.snippet.thumbnails.high.url
+    :width=this.size.width
+    :height=this.size.imgHeight
+    alt="Responsive image" />
+    <div v-if="this.$store.state.videos.playingId === video.id"
+    :width=this.size.width
+    :height=this.size.height>
+      <youtube
+      :video-id="video.id"
+      :player-vars=this.opt
+      :player-width=this.size.width
+      :player-height=this.size.height
+      @ready="ready"
+      @ended="ended"
+      @playing="playing"
+      @error="error"
+      ></youtube>
+    </div>
     <div class="d-flex w-100 justify-content-between">
         <h6 class="videocardtitle">{{ this.video.snippet.localized.title }}</h6>
     </div>
@@ -14,6 +31,26 @@ import { mapState, mapActions } from 'vuex';
 export default {
   name: 'VideoCard',
   props: { video: Object },
+  data: function data() {
+    return {
+      size: {
+        width: 0,
+        height: 0,
+        imgHeight: 0,
+      },
+      opt: {
+        autoplay: 1,
+        playsinline: 1,
+      },
+      player: undefined,
+    };
+  },
+  mounted() {
+    this.size.width = this.$el.clientWidth;
+    const ratio = 360 / 480;
+    this.size.height = this.size.width * ratio;
+    this.size.imgHeight = this.size.height + 6.250;
+  },
   computed: {
     ...mapState({
     }),
@@ -22,6 +59,15 @@ export default {
     ...mapActions('videos', [
       'play',
     ]),
+    ready(event) {
+      this.player = event.target;
+    },
+    ended() {
+    },
+    error() {
+    },
+    playing() {
+    },
   },
 };
 </script>
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="videolist">
-    <virtual-list :size="elementHeight" :remain="4" :bench="8"
-    :onscroll="handleScroll" :debounce="300">
-        <video-card v-for="(video, index) of videos" class="videolistitem" v-bind='{ video }'
-        v-bind:index="index" v-bind:key="video.id" />
-    </virtual-list>
+    <RecycleScroller
+    class="scroller"
+    :items="videos"
+    :item-height="340">
+      <div slot-scope='{ item, index }'>
+        <video-card class="videolistitem" :video=item />
+      </div>
+    </RecycleScroller>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
-import virtualList from 'vue-virtual-scroll-list';
+import { RecycleScroller } from 'vue-virtual-scroller';
 import VideoCard from './VideoCard';
 
 export default {
   name: 'VideoList',
-  components: { VideoCard, 'virtual-list': virtualList },
+  components: { VideoCard, RecycleScroller },
   computed: mapState({
     videos: state => state.videos.all,
     playingId: state => state.videos.playingId,
@@ -23,21 +26,23 @@ export default {
   created() {
     this.$store.dispatch('videos/getAllVideos');
   },
+  mounted() {
+  },
   methods: {
     ...mapActions('videos', [
       'play',
     ]),
-    handleScroll: function handleScroll(event, data) {
-      const idx = Math.ceil(data.offset / this.$data.elementHeight);
-      const videoId = this.videos[idx].id;
-      if (this.playingId !== videoId) {
-        this.play(videoId);
-      }
-    },
+    // handleScroll: function handleScroll(event, data) {
+    //   const idx = Math.ceil(data.offset / this.$data.elementHeight);
+    //   const videoId = this.videos[idx].id;
+    //   if (this.playingId !== videoId) {
+    //     this.play(videoId);
+    //   }
+    // },
   },
   data: function data() {
     return {
-      elementHeight: 328,
+      elementHeight: 340,
     };
   },
 };
@@ -45,6 +50,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.scroller {
+  height: 100%;
+}
 .videolistitem {
   background-color:#343a40;
   padding: 4px;
