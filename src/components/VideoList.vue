7e05c88da4c162279ee89cc5a26394e9cd7fdc14
daewook <logue777@nate.com>
2018-12-20 22:37:04 +0900

    스크롤해서 이벤트 날리기

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="videolist">
-    <virtual-list :size="328" :remain="4" :bench="8" :onscroll="handleScroll">
+    <virtual-list :size="elementHeight" :remain="4" :bench="8" :onscroll="handleScroll" :debounce="300">
         <video-card v-for="(video, index) of videos" class="videolistitem" v-bind='{ video }'
         v-bind:index="index" v-bind:key="video.id" />
     </virtual-list>
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 import virtualList from 'vue-virtual-scroll-list';
 import VideoCard from './VideoCard';
 
@@ -17,14 +17,27 @@ export default {
   components: { VideoCard, 'virtual-list': virtualList },
   computed: mapState({
     videos: state => state.videos.all,
+    playingId: state => state.videos.playingId,
   }),
   created() {
     this.$store.dispatch('videos/getAllVideos');
   },
   methods: {
+    ...mapActions('videos', [
+      'play',
+    ]),
     handleScroll: function handleScroll(event, data) {
-      console.log(event, data);
+      let idx = Math.ceil(data.offset / this.$data.elementHeight);
+      var videoId = this.videos[idx].id;
+      if (this.playingId !== videoId) {
+        this.play(videoId);
+      }
     },
+  },
+  data: function data() {
+    return {
+      elementHeight: 328,
+    };
   },
 };
 </script>
